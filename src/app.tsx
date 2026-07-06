48813fdfb4b3fdc81730a7059ab869cd9156826c
lehaeiin <{alkane0410@naver.com}>
2026-07-06 14:00:54 +0900

    ✨ GuideStep(tutorial, 산책시작시, 산책 중 안내사항)
    로그인 후 어플 안내, 산책 시작 시 안내사항, 산책 중 조건 안내 모달 총 9(3,3,3)페이지 구현

--- src/app.tsx
+++ src/app.tsx
@@ -8,6 +8,10 @@ import ProtectedRoute from './routes/protectedRoute'
 import { useAuthStore } from './stores/authStore'
 import DogRegisterPage from './pages/onboarding/dogRegisterPage'
 import TerritoryColorPage from './pages/onboarding/territoryColorPage'
+import ServiceIntroPage from './pages/onboarding/serviceIntroPage'
+import HomePage from './pages/home/homePage'
+import WalkReadyPage from './pages/walk/walkReadyPage'
+import WalkActivePage from './pages/walk/walkActivePage'
 
 export default function App() {
   const initializeAuth = useAuthStore((state) => state.initializeAuth)
@@ -26,6 +30,10 @@ export default function App() {
         <Route element={<ProtectedRoute />}>
           <Route path={ROUTES.ONBOARDING.DOG} element={<DogRegisterPage />} />
           <Route path={ROUTES.ONBOARDING.COLOR} element={<TerritoryColorPage />} />
+          <Route path={ROUTES.ONBOARDING.TUTORIAL} element={<ServiceIntroPage />} />
+          <Route path={ROUTES.HOME} element={<HomePage />} />
+          <Route path={ROUTES.WALK.READY} element={<WalkReadyPage />} />
+          <Route path={ROUTES.WALK.ACTIVE} element={<WalkActivePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
